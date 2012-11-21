12b14ed902d440cce1abfbc625b6be856411e614
Mico Loretan <loretan.mico@gmail.com>
2012-11-21 13:02:16 -0500

    version 0.154

--- selnolig.lua
+++ selnolig.lua
@@ -6,7 +6,7 @@
 -- Author: Mico Loretan (loretan dot mico at gmail dot com)
 --    (with crucial contributions by Taco Hoekwater, 
 --    Patrick Gundlach, and Steffen Hildebrandt)
--- Date: 2012/11/19
+-- Date: 2012/11/21
 --
 -- This entire package is placed under the terms of the
 -- LaTeX Project Public License, version 1.3 or later
