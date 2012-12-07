6da8a8745a02e01c012f7ded9b04803f0479febd
Mico Loretan <loretan.mico@gmail.com>
2012-12-06 19:38:11 -0500

    0.159
    Found a kludgy but effective solution for problems that arise if
    there's a character with an Umlaut (dieresis) in the first part of the
    replacement string of \nolig command.

--- selnolig.lua
+++ selnolig.lua
@@ -6,7 +6,7 @@
 -- Author: Mico Loretan (loretan dot mico at gmail dot com)
 --    (with crucial contributions by Taco Hoekwater, 
 --    Patrick Gundlach, and Steffen Hildebrandt)
--- Date: 2012/12/06
+-- Date: 2012/12/07
 --
 -- The entire selnolig package is placed under the terms 
 -- of the LaTeX Project Public License, version 1.3 or 
