1cc72a2f004c01fe9b6a2eeea4caedc22b33b9af
Mico Loretan <loretan.mico@gmail.com>
2013-01-13 14:55:03 -0500

    0.169c
    removed f_t.alt ligature from user guide; minor fixes here and there

--- selnolig.lua
+++ selnolig.lua
@@ -6,7 +6,7 @@
 -- Author: Mico Loretan (loretan dot mico at gmail dot com)
 --    (with crucial contributions of Taco Hoekwater, 
 --    Patrick Gundlach, and Steffen Hildebrandt)
--- Date: 2013/01/11
+-- Date: 2013/01/13
 --
 -- The entire selnolig package is placed under the terms 
 -- of the LaTeX Project Public License, version 1.3 or 
